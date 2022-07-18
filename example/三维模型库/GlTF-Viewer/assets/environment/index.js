2f7ee056edb19a7868cba7d20d6f9405287b7526
hubzyy <zy.jpeg@gmail.com>
2022-07-18 10:44:04 +0800

    添加素材

---
+++ example/三维模型库/GlTF-Viewer/assets/environment/index.js
@@ -0,0 +1,26 @@
+export const environments = [
+  {
+    id: '',
+    name: 'None',
+    path: null,
+    format: '.hdr'
+  },
+  {
+    id: 'venice-sunset',
+    name: 'Venice Sunset',
+    path: 'assets/environment/venice_sunset_1k.hdr',
+    format: '.hdr'
+  },
+  {
+    id: 'footprint-court',
+    name: 'Footprint Court (HDR Labs)',
+    path: 'assets/environment/footprint_court_2k.hdr',
+    format: '.hdr'
+  },
+  {
+    id: 'Indoor-bathroom',
+    name: 'Indoorbathroom',
+    path: 'assets/environment/Indoor_bathroom.hdr',
+    format: '.hdr'
+  }
+];
